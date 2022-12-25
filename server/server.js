e17c376979c87ceda04a6ab385977647676b8923
Alia2006 <luanlinxue@gmail.com>
2022-12-25 21:55:19 +1300

    change server port to 5000

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5003, () => console.log('AI server started on http://localhost:5003'))
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
