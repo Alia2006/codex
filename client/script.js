d4b06a9918e00f98641d9e70bebb06eacc3e941a
Alia2006 <luanlinxue@gmail.com>
2023-01-02 18:28:39 +1300

    update url

--- client/script.js
+++ client/script.js
@@ -86,8 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-anxu.onrender.com', {
-
+    const response = await fetch('https://chatgpt-qde4.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
